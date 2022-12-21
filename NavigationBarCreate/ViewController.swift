dd7ee4b56c1a3715eef09e1ff325c1a259e075a8
Di1105 <delcioglu@gmail.com.com>
2022-12-21 17:06:47 +0300

    First Commit

--- NavigationBarCreate/ViewController.swift
+++ NavigationBarCreate/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    let navigationBar = UINavigationBar()
+    let navigataionItem = UINavigationItem()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpUI()
     }
 
-
+    func setUpUI(){
+        view.addSubview(navigationBar)
+        
+        navigationBar.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(40)
+            make.leading.equalTo(view.snp.leading)
+            make.trailing.equalTo(view.snp.trailing)
+        }
+        navigationBar.backgroundColor = .lightGray
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Left", style: .plain, target: self, action: nil)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Right", style: .plain, target: self, action: nil)
+        navigationBar.setItems([navigationItem], animated: false)
+        navigationItem.title = "Navi Bar"
+    }
 }
 
